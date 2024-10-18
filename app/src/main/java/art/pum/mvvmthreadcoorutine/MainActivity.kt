b3a3548ca9a89b8c6096e706399ecd43181b0aac
artur <artur.arciuch@gmail.com>
2024-10-18 09:08:48 +0200

    warianty bindingu

--- app/src/main/java/art/pum/mvvmthreadcoorutine/MainActivity.kt
+++ app/src/main/java/art/pum/mvvmthreadcoorutine/MainActivity.kt
@@ -3,7 +3,9 @@ package art.pum.mvvmthreadcoorutine
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import androidx.databinding.DataBindingUtil
+import androidx.lifecycle.ViewModel
 import androidx.lifecycle.ViewModelProvider
+import androidx.lifecycle.viewmodel.compose.viewModel
 import art.pum.mvvmthreadcoorutine.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
@@ -15,7 +17,13 @@ class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
 
+        //tak
         binding = DataBindingUtil.setContentView(this, R.layout.activity_main)
+
+        //albo tak:
+        //binding = ActivityMainBinding.inflate(layoutInflater)
+        //setContentView(binding.root)
+
         myViewModel = ViewModelProvider(this)[MyViewModel::class.java]
 
         binding.viewModel = myViewModel
